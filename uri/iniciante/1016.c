b6572f00985ee2414eb9c3e53a0c5d17a9086192
vince0417 <vicenterodrigues56438@gmail.com>
2022-08-15 19:27:02 -0300

    Distância

---
+++ uri/iniciante/1016.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int minutos;
+    int distancia;
+    scanf("%d", &distancia);
+    minutos = distancia + distancia;
+    printf("%d minutos\n", minutos);
+    return 0;
+}
