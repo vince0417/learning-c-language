bc4d52a673e44f2ae2bdc2ae4e34e2339a39cf26
vince0417 <vicenterodrigues56438@gmail.com>
2022-08-15 19:25:53 -0300

    Consumo

---
+++ uri/iniciante/1014.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    float b;
+    double consumo;
+    scanf("%d %f", &a, &b);
+    consumo = a / b;
+    printf("%.3lf km/l\n", consumo);
+    return 0;
+}
