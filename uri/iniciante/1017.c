bd05300f3c313dfbd1dad63fda57cd5009141cd9
vince0417 <vicenterodrigues56438@gmail.com>
2022-08-15 19:28:01 -0300

    Gasto de Combustível

---
+++ uri/iniciante/1017.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float tempo, velocidade;
+    float distancia;
+    double combustivel;
+    scanf("%f", &tempo);
+    scanf("%f", &velocidade);
+    distancia = tempo * velocidade;
+    combustivel = distancia / 12;
+    printf("%.3lf\n", combustivel);
+    return 0;
+}
