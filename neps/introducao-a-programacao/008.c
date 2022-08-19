07b3e440d31542e1d650fdd345c5089ed4b69a5a
vince0417 <vicenterodrigues56438@gmail.com>
2022-08-18 22:51:47 -0300

    Repetir X Vezes

---
+++ neps/introducao-a-programacao/008.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    int y;
+    scanf("%d", &x);
+
+    for (y = 1; y <= x; y = y + 1)
+    {
+        printf("NepsAcademy eh Sucesso\n");
+    }
+
+    return 0;
+}
