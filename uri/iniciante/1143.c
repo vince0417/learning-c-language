3c8af999f73f2ad4c84a5980c3773ec96e1798d2
vince0417 <vicenterodrigues56438@gmail.com>
2022-10-15 15:26:49 -0300

    Quadrado e ao Cubo

---
+++ uri/iniciante/1143.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int x;
+    scanf("%d", &x);
+
+    for (int i = 1; i <= x; i++)
+    {
+        int a, b;
+        a = i * i;
+        b = i * a;
+        printf("%i %i %i\n", i, a, b);
+    }
+
+    return 0;
+}
